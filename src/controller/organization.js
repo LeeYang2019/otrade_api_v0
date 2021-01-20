35cf0e24ddcf821b9187e8909d4ccfb97280fadf
leeyang2019 <nyang@madisoncollege.edu>
2021-01-20 08:27:22 -0500

    refactored code

--- src/controller/organization.js
+++ src/controller/organization.js
@@ -63,34 +63,6 @@ exports.addOrganization = asyncHandler(async (req, res, next) => {
 	req.body.project = req.params.projectId;
 	req.body.user = req.user.id;
 
-	console.log(req.body);
-
-	// we use Joi... Hapi-joi
-	// for input adapter
-	/*
-		let schemaV1 = Joi.Object.keys({
-			name: Joi.String().min().max().required(),
-			owner: Joi.String().min().max().required(),
-			startDate: Joi.String().min().max().required()
-		})
-
-		let schemaV2 = Joi.Object.keys({
-			firstName: Joi.String().min().max().required(),
-			owner: Joi.String().min().max().required(),
-			startDate: Joi.String().min().max().required(),
-			location: Joi.String().min().max().optional()
-		})
-
-		let schema = undefined;
-		if(req.body.apiVersion === "v1") schema = schemaV1;
-		else schema = schemaV2;
-
-		let result = await Joi.validate(req.body, schema);
-		if(result.error) {
-			res.status(400).send(result.error);
-		}
-	 */
-
 	const organization = await Organization.create(req.body);
 	res.status(200).json({ success: true, data: organization });
 });
