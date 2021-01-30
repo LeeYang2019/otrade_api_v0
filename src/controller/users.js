cff8cd1b405421837bda11c3a1f868dbb9f4fbc8
leeyang2019 <nyang@madisoncollege.edu>
2021-01-30 16:45:27 -0500

    refactored code

--- src/controller/users.js
+++ src/controller/users.js
@@ -15,20 +15,25 @@ const mongoose = require('mongoose');
 exports.authMe = asyncHandler(async (req, res) => {
 	const { email, password } = req.body;
 
-	const user = await User.findOne({ email });
+	try {
+		const user = await User.findOne({ email });
 
-	if (user && (await user.matchPassword(password))) {
-		res.json({
-			_id: user._id,
-			firstName: user.firstName,
-			lastName: user.lastName,
-			email: user.email,
-			role: user.role,
-			token: generateToken(user._id),
-		});
-	} else {
-		res.status(401); //unauthorized
-		throw new Error('Invalid email or password');
+		//if the user exists and password matches, return the user
+		if (user && (await user.matchPassword(password))) {
+			res.json({
+				_id: user._id,
+				firstName: user.firstName,
+				lastName: user.lastName,
+				email: user.email,
+				role: user.role,
+				token: generateToken(user._id),
+			});
+		} else {
+			res.status(401); //unauthorized
+			throw new Error('Invalid email or password');
+		}
+	} catch (error) {
+		res.status(500).json(error.message);
 	}
 });
 
@@ -36,22 +41,26 @@ exports.authMe = asyncHandler(async (req, res) => {
 // @route   GET /api/v1/users/profile
 // @access  private
 exports.getMyUserProfile = asyncHandler(async (req, res) => {
-	//auth middleware passes user here
-	const user = await User.findById(req.user._id);
+	try {
+		//auth middleware passes user here
+		const user = await User.findById(req.user._id);
 
-	//matches enteredPassword with db user password
-	if (user) {
-		res.json({
-			_id: user._id,
-			firstName: user.firstName,
-			lastName: user.lastName,
-			telephone: user.telephone,
-			email: user.email,
-			role: user.role,
-		});
-	} else {
-		res.status(401); //unauthorized
-		throw new Error('Invalid email or password');
+		//matches enteredPassword with db user password
+		if (user) {
+			res.json({
+				_id: user._id,
+				firstName: user.firstName,
+				lastName: user.lastName,
+				telephone: user.telephone,
+				email: user.email,
+				role: user.role,
+			});
+		} else {
+			res.status(401); //unauthorized
+			throw new Error('Invalid email or password');
+		}
+	} catch (error) {
+		res.status(500).json(error.message);
 	}
 });
 
@@ -59,36 +68,40 @@ exports.getMyUserProfile = asyncHandler(async (req, res) => {
 // @route   PUT /api/v1/users/profile
 // @access  private
 exports.updateMyUserProfile = asyncHandler(async (req, res) => {
-	//auth middleware passes user here
-	const user = await User.findById(req.user._id);
+	try {
+		//auth middleware passes user here
+		const user = await User.findById(req.user._id);
 
-	//matches enteredPassword with db user password
-	if (user) {
-		user.firstName = req.body.firstName || user.firstName;
-		user.lastName = req.body.lastName || user.lastName;
-		user.telephone = req.body.telephone || user.telephone;
-		user.image = req.body.image || user.image;
-		user.email = req.body.email || user.email;
+		//matches enteredPassword with db user password
+		if (user) {
+			user.firstName = req.body.firstName || user.firstName;
+			user.lastName = req.body.lastName || user.lastName;
+			user.telephone = req.body.telephone || user.telephone;
+			user.image = req.body.image || user.image;
+			user.email = req.body.email || user.email;
 
-		if (req.body.password) {
-			user.password = req.body.password;
+			if (req.body.password) {
+				user.password = req.body.password;
+			}
+
+			const updatedUser = await user.save();
+
+			res.json({
+				_id: updatedUser._id,
+				firstName: updatedUser.firstName,
+				lastName: updatedUser.lastName,
+				email: updatedUser.email,
+				telephone: updatedUser.telephone,
+				status: updatedUser.status,
+				role: updatedUser.role,
+				token: generateToken(updatedUser._id),
+			});
+		} else {
+			res.status(401); //unauthorized
+			throw new Error('user not found');
 		}
-
-		const updatedUser = await user.save();
-
-		res.json({
-			_id: updatedUser._id,
-			firstName: updatedUser.firstName,
-			lastName: updatedUser.lastName,
-			email: updatedUser.email,
-			telephone: updatedUser.telephone,
-			status: updatedUser.status,
-			role: updatedUser.role,
-			token: generateToken(updatedUser._id),
-		});
-	} else {
-		res.status(401); //unauthorized
-		throw new Error('user not found');
+	} catch (error) {
+		res.status(500).json(error.message);
 	}
 });
 
@@ -100,33 +113,48 @@ exports.updateMyUserProfile = asyncHandler(async (req, res) => {
 // @route   GET /api/v1/users
 // @access  Private/admin
 exports.getUsers = asyncHandler(async (req, res) => {
-	const pageSize = 5;
-	const page = Number(req.query.pageNumber) || 1;
+	try {
+		const pageSize = 5;
+		const page = Number(req.query.pageNumber) || 1;
 
-	const keyword = req.query.keyword
-		? {
-				lastName: {
-					$regex: req.query.keyword,
-					$options: 'i',
-				},
-		  }
-		: {};
+		const keyword = req.query.keyword
+			? {
+					lastName: {
+						$regex: req.query.keyword,
+						$options: 'i',
+					},
+			  }
+			: {};
 
-	const count = await User.countDocuments({ ...keyword });
-	const users = await User.find({ ...keyword })
-		.select('-password')
-		.sort({ lastName: 1 })
-		.limit(pageSize)
-		.skip(pageSize * (page - 1));
-	res.status(200).json({ users, page, pages: Math.ceil(count / pageSize) });
+		const count = await User.countDocuments({ ...keyword });
+		const users = await User.find({ ...keyword })
+			.select('-password')
+			.sort({ lastName: 1 })
+			.limit(pageSize)
+			.skip(pageSize * (page - 1));
+		res.status(200).json({ users, page, pages: Math.ceil(count / pageSize) });
+	} catch (error) {
+		res.status(500).json(error.message);
+	}
 });
 
 // @desc    Get a user
 // @route   GET /api/v1/users/:id
 // @access  Private/admin
 exports.getUser = asyncHandler(async (req, res) => {
-	const user = await User.findById(req.params.id);
-	res.status(200).json({ success: true, data: user });
+	try {
+		const user = await User.findById(req.params.id);
+
+		if (!user) {
+			res.status(401);
+			throw new Error('No resource found');
+		}
+
+		//return the user
+		res.status(200).json({ success: true, data: user });
+	} catch (error) {
+		res.status(500).json(error.message);
+	}
 });
 
 // @desc    Register a new user
@@ -135,32 +163,37 @@ exports.getUser = asyncHandler(async (req, res) => {
 exports.registerUser = asyncHandler(async (req, res) => {
 	const { email } = req.body;
 
-	const userExists = await User.findOne({ email });
+	try {
+		// lookup user with email
+		const userExists = await User.findOne({ email });
 
-	if (userExists) {
-		res.status(404);
-		throw new Error('User already exists');
-	}
+		if (userExists) {
+			res.status(404);
+			throw new Error('User already exists');
+		}
 
-	req.body.user = req.user;
+		req.body.user = req.user;
 
-	//create user
-	const user = await User.create(req.body);
+		//create user
+		const user = await User.create(req.body);
 
-	if (user) {
-		res.status(201).json({
-			_id: user._id,
-			firstName: user.firstName,
-			lastName: user.lastName,
-			email: user.email,
-			image: user.image,
-			role: user.role,
-			status: user.status,
-			token: generateToken(user._id),
-		});
-	} else {
-		res.status(404);
-		throw new Error('User not found');
+		if (user) {
+			res.status(201).json({
+				_id: user._id,
+				firstName: user.firstName,
+				lastName: user.lastName,
+				email: user.email,
+				image: user.image,
+				role: user.role,
+				status: user.status,
+				token: generateToken(user._id),
+			});
+		} else {
+			res.status(404);
+			throw new Error('User not found');
+		}
+	} catch (error) {
+		res.status(500).json(error.message);
 	}
 });
 
@@ -168,27 +201,29 @@ exports.registerUser = asyncHandler(async (req, res) => {
 // @route   PUT /api/v1/admin/users/:id
 // @access  Private/admin
 exports.updateUser = asyncHandler(async (req, res) => {
-	console.log('code enters here');
-	const user = await User.findById(req.params.id);
+	try {
+		const user = await User.findById(req.params.id);
 
-	//matches enteredPassword with db user password
-	if (user) {
-		user.firstName = req.body.firstName || user.firstName;
-		user.lastName = req.body.lastName || user.lastName;
-		user.email = req.body.email || user.email;
-		user.image = req.body.image || user.image;
-		user.telephone = req.body.telephone || user.telephone;
-		user.status = req.body.status || user.status;
-		user.role = req.body.role || user.role;
+		//matches enteredPassword with db user password
+		if (user) {
+			user.firstName = req.body.firstName || user.firstName;
+			user.lastName = req.body.lastName || user.lastName;
+			user.email = req.body.email || user.email;
+			user.image = req.body.image || user.image;
+			user.telephone = req.body.telephone || user.telephone;
+			user.status = req.body.status || user.status;
+			user.role = req.body.role || user.role;
+			user.image = req.body.image || user.image;
 
-		const updatedUser = await user.save();
-
-		res.json({
-			updatedUser,
-		});
-	} else {
-		res.status(401); //unauthorized
-		throw new Error('user not found');
+			//save user
+			const updatedUser = await user.save();
+			res.status(201).json({ updatedUser });
+		} else {
+			res.status(401); //unauthorized
+			throw new Error('user not found');
+		}
+	} catch (error) {
+		res.status(500).json(error.message);
 	}
 });
 
